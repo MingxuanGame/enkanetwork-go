c13135440c989949b7badb89bf49296f69d28a49
MingxuanGame <MingxuanGame@outlook.com>
2022-07-30 00:35:54 +0800

    :bug: add User-Agent for request

--- api/api.go
+++ api/api.go
@@ -12,7 +12,10 @@ import (
 
 // GetInfo requests Enka.Network and returns the EnkaNetworkData structure, or nil if there is an error
 func GetInfo(uid uint32) (data *model.EnkaNetworkData, err error) {
-	resp, err := http.Get(fmt.Sprintf("https://enka.network/u/%d/__data.json", uid))
+	client := &http.Client{}
+	request, _ := http.NewRequest("GET", fmt.Sprintf("https://enka.network/u/%d/__data.json", uid), nil)
+	request.Header.Add("User-Agent", "EnkaNetwork-Go/0.1.0")
+	resp, err := client.Do(request)
 	if err != nil {
 		return nil, err
 	}
